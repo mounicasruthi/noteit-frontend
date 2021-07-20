92bf87cd130d07a66368b69a893b0fc08e0cfcf3
mounicasruthi <mounicasruthi30@gmail.com>
2021-07-20 20:58:23 +0530

    modified pages

--- home/script.js
+++ home/script.js
@@ -1,1 +1,10 @@
+const registerButton = document.querySelector(".link");
+
+
+registerButton.addEventListener("click", () => {
+  location.href = "/signup/index.html";
+});
+
+
+
 particlesJS.load("particles-js", "particlesjs-config.json");
--- notes/script.js
+++ notes/script.js
@@ -25,17 +25,17 @@ const cardContainer = document.querySelector(".card-container");
 
 const cardData = [
   {
-    heading: "heading1",
+    heading: "heading 1",
     content:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure sequi culpa officiis quae, quod aperiam temporibus pariatur, est laboriosam corporis similique laudantium repellat quas expedita possimus tempora provident doloremque illum exercitationem, architecto deserunt. Fuga repellat incidunt assumenda dolore cumque nihil facilis repudiandae? Explicabo aspernatur earum nostrum amet aperiam, ab distinctio!",
     id: 1,
   },
-  { heading: "heading2", content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", id: 2 },
-  { heading: "heading3", content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", id: 3 },
-  { heading: "heading4", content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", id: 4 },
-  { heading: "heading5", content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", id: 5 },
-  { heading: "heading6", content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", id: 6 },
-  { heading: "heading7", content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", id: 7 },
+  { heading: "heading 2", content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", id: 2 },
+  { heading: "heading 3", content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", id: 3 },
+  { heading: "heading 4", content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", id: 4 },
+  { heading: "heading 5", content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", id: 5 },
+  { heading: "heading 6", content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", id: 6 },
+  { heading: "heading 7", content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", id: 7 },
 ];
 
 const createNotes = (array) => {
@@ -46,7 +46,7 @@ const createNotes = (array) => {
     card.classList.add("card");
     card.id = id;
 
-    const insideHtml = `<div class="card-header"><div class="card-heading">${heading}</div><a href="https://updatenote.netlify.app/?noteId=${id}"><div class="edit-note"><img src="edit-note.svg" alt="" /></div></a></div><div class="card-content">${content}</div>`;
+    const insideHtml = `<div class="card-header"><div class="card-heading">${heading}</div><a href="https://updatenote.netlify.app/?noteId=${id}"><div class="edit-note"><img src="../assets/Images/editIcon.svg" alt="" /></div></a></div><div class="card-content">${content}</div>`;
 
     card.innerHTML = insideHtml;
 
