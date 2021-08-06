97422fa1cf50385d3d50c8bf6ca205ac39cff4b7
Mounica <mounica@pop-os.localdomain>
2021-08-06 06:41:53 +0530

    modified pages

---
+++ script.js
@@ -0,0 +1,10 @@
+const body = document.querySelector("body");
+const registerButton = document.querySelector(".link");
+
+
+registerButton.addEventListener("click", () => {
+  location.href = "/signup/index.html";
+});
+
+
+particlesJS.load("particles-js", "particlesjs-config.json");
